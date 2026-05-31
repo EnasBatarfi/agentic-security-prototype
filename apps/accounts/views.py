fd5a28c5192452afa78a9733ea5a15469b62c364
EnasBatarfi <Enasmbatarfi@gmail.com>
2026-05-31 20:54:52 +0300

    Add email field to signup form

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,14 +1,18 @@
 from django.contrib.auth import login
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
 
+class SignUpForm(UserCreationForm):
+    """Form for creating a new user account."""
+    email = forms.EmailField(required=True)
 
 def signup(request):
     """Create a user account, then log the user in."""
 
     # If the request is a POST then the user has signed up
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
 
         # Check if the form is valid and if so save the user and log them in
         if form.is_valid():
@@ -17,6 +21,6 @@ def signup(request):
             return redirect("file_list")
     # Otherwise, the user is just viewing the signup page so show them the form
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
 
     return render(request, "accounts/signup.html", {"form": form})
